f6632f178aeb6905467ff6c1492850423bdaf3fc
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2023-09-20 17:12:37 +0100

    Update newRequest.js

--- Client/src/utils/newRequest.js
+++ Client/src/utils/newRequest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
- // baseURL: "http://localhost:8800/api",
-  baseURL:" https://phaxnetgigs.onrender.com/api",
+  baseURL: "https://phaxnetgigs.onrender.com/api",
   withCredentials: true,
 });
 
