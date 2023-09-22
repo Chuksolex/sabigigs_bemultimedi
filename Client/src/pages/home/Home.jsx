30777442e9e68607f60d47b1fa716d2e78650374
Chukwuma Oleka <chuks4flourish@gmail.com>
2023-09-22 11:06:36 +0100

    Removed the Trusted By from Home, bc it causing some layout issues

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react';
 import {useQuery} from "@tanstack/react-query";
 import "./Home.scss";
 import Featured from '../../components/featured/Featured';
-import TrustedBy from '../../components/trustedBy/TrustedBy';
+//import TrustedBy from '../../components/trustedBy/TrustedBy';
 //import Slide from '../../components/slide/Slide';
 import CategoryCard from '../../components/categoryCard/CategoryCard';
 import  ProjectCard from "../../components/projectCard/ProjectCard";
@@ -44,7 +44,7 @@ const Home = () => {
  
     <div className='home'>
       <Featured />
-      <TrustedBy />
+      {/* <TrustedBy /> */}
      
       
       <div className='container  category'>
