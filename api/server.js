c538d30aa2499d45c65c7d07908b7973f3c3a76f
Chukwuma Oleka <chuks4flourish@gmail.com>
2023-09-23 16:32:15 +0100

    change the app.use to be before cors

--- api/server.js
+++ api/server.js
@@ -59,12 +59,12 @@ const connect = async () => {
       store: store,
     })
   );
+
+
+
   app.use(express.json());
   app.use(cookieParser());
   bodyParser.json();
-
-
-
 
 //app.use(cors({origin:"http://localhost:5173", credentials:true} ));
 app.use(cors({
