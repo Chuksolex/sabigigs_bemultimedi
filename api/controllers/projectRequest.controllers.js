b39bbf3d9ca9ff506cc2bbfec1983b0a8304a19b
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-27 12:03:37 +0100

    Create projectRequest.controllers.js

---
+++ api/controllers/projectRequest.controllers.js
@@ -0,0 +1,44 @@
+import express from "express"
+import ProjectRequest from "../models/projectRequest.model.js";
+import createError from "../utils/createError.js";
+import sendNotificationEmail from "../utils/sendNotificationEmail.js";
+
+
+export const createProjectRequest = async (req, res, next) => {
+try {
+    const {
+      name,
+      email,
+      phone,
+      projectDescription,
+      startDate,
+      budget
+    } = req.body;
+
+    const projectRequest = new ProjectRequest({
+      name,
+      email,
+      phone,
+      projectDescription,
+      startDate,
+      budget
+    });
+  const recipientEmail= "chuks4flourish@gmail.com";
+  const subject = "Project Request";
+  const message = `User details:  (${name} ${email} ${phone}). Message: (${projectDescription}). Poposed start date: (${startDate}). Budget: (${budget).`
+  
+    await projectRequest.save();
+
+
+    // Send password reset email
+    sendNotificationEmail(recipientEmail, subject, message);
+
+    res.status(200).json('Got new project reequest from Prettygigs contact form.');
+  } catch (error) {
+    next(error);
+    console.log(error)
+  }
+};
+
+
+
