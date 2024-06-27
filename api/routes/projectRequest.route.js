cd9831312a29d94b3754a29764263f07f3d3614a
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-27 12:13:42 +0100

    Create projectRequest.route.js

---
+++ api/routes/projectRequest.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {createProjectRequest } from "../controllers/projectRequest.controllers.js";
+
+
+
+const projectRequestRoute = express.Router();
+
+authRoute.post("/projectRequest", createProjectRequest};
+
+export default projectRequestRoute;
