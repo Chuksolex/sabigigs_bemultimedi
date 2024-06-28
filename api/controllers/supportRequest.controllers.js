cb6fc5cfdf791dc6c0cf762b808e553980003830
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-28 18:08:31 +0100

    Create supportRequest.controllers.js

---
+++ api/controllers/supportRequest.controllers.js
@@ -0,0 +1,32 @@
+import SupportRequest from "../models/supportRequest.model.js";
+import sendNotificationEmail from "../utils/sendNotificationEmail.js";
+import User from "../models/user.model.js";
+
+const createSupportRequest = async (req, res) => {
+  try {
+    const { title, description, attachmentUrl, user } = req.body;
+    const person = await User.findById(user);
+
+    if(!person){
+      res.send("No user with your details on our data base");
+    }
+    const newSupportRequest = new SupportRequest({
+      title,
+      description,
+      attachmentUrl,
+      user, // Save the user ID with the request
+    });
+
+    await newSupportRequest.save();
+    
+
+    await sendNotificationEmail(recipientEmail:"chuks4flourish@gmail.com", subject: "Support Request", message: description);
+        await sendNotificationEmail(recipientEmail: `${person.email}`, subject: "We Got Your Support Request", message: `Dear ${person.name}, we have received your request and shall respond in the earliest possible time.`);
+
+
+    res.status(200).json({ message: 'Support request submitted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to submit support request' });
+  }
+};
+export default createSupportRequest;
