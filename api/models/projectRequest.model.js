0ce41d0ab58123d264ec62bd27ce58243ecccf42
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-28 01:41:57 +0100

    Update projectRequest.model.js

--- api/models/projectRequest.model.js
+++ api/models/projectRequest.model.js
@@ -4,7 +4,16 @@ const { Schema } = mongoose;
 const projectRequestSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: {
+    number: { type: String, required: true },
+    country: {
+      name: { type: String, required: true },
+      dialCode: { type: String, required: true },
+      countryCode: { type: String, required: true },
+      format: { type: String, required: true },
+    },
+    formattedValue: { type: String, required: true },
+  },
   projectDescription: { type: String, required: true },
   startDate: { type: Date, required: false },
   budget: { type: Number, required: false },
