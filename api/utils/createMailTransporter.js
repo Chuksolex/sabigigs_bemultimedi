988ae97822a48c70d131f58d585ee2e138a1e86b
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-28 04:18:42 +0100

    Update createMailTransporter.js

--- api/utils/createMailTransporter.js
+++ api/utils/createMailTransporter.js
@@ -1,19 +1,16 @@
 import nodemailer from "nodemailer";
-
+import dotenv from "dotenv";
+// Create a transporter object
 const createMailTransporter = async () => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: 'chukwumaoleka@outlook.com',
-      pass: 'Fadama1@biz', // Replace with your actual password or app-specific password
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  return transporter;
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // use false for STARTTLS; true for SSL on port 465
+  auth: {
+    user: `${process.env.GMAIL_ACCOUNT}`,
+    pass: `${process.env.Nodemailer_App_Password}`,
+  }
+});
+   return transporter;
 };
 export default createMailTransporter
