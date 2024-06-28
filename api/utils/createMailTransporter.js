84f51341b4b3378a7223044802586ef643c66ada
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-28 03:19:59 +0100

    Update createMailTransporter.js

--- api/utils/createMailTransporter.js
+++ api/utils/createMailTransporter.js
@@ -1,22 +1,18 @@
 import nodemailer from "nodemailer";
 
-// Create a Nodemailer transporter
- const createMailTransporter = async() => {
-     const transporter = nodemailer.createTransport({
-      service: "Outlook",
-      auth: {
-        user: 'chukwumaoleka@outlook.com',
-        pass: 'Fadama1@biz',
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-      //secure: false,
-      debug: true,
+const createMailTransporter = async () => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'chukwumaoleka@outlook.com',
+      pass: 'Fadama1@biz', // Replace with your actual password or app-specific password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-     
-    });
-    return transporter;
- 
-  }
-  export default createMailTransporter  ;
+  return transporter;
+};
