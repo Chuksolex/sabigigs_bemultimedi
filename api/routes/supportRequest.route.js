56e3e80782046da935370d9a31a25b011d2f3209
Chukwuma Oleka <75509134+Chuksolex@users.noreply.github.com>
2024-06-28 18:12:47 +0100

    Create supportRequest.route.js

---
+++ api/routes/supportRequest.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {createSupportRequest} from "../controllers/supportRequest.controllers.js";
+
+
+
+const supportRequestRoute = express.Router();
+
+supportRequestRoute.post("/", createSupportRequest);
+
+
+
+
+
+
+
+export default supportRequestRoute;
